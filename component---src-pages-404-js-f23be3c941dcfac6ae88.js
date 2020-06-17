74ce3c95793d0dc0b3f97faefa6c812961023749
manzanillo <manzanillo@users.noreply.github.com>
2020-06-17 11:51:31 +0000

    deployed via Gatsby Publish Action 🎩 for bd8dcaef8ce58aaf94b7dbc6f9c8954be1af9bd0

---
+++ component---src-pages-404-js-f23be3c941dcfac6ae88.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),r=t("Bl7J"),o=t("vrFN");n.default=function(){return l.a.createElement(r.a,null,l.a.createElement(o.a,{title:"404: Not found"}),l.a.createElement("h1",null,"Page not Found :("),l.a.createElement("p",null,"Try another page."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-f23be3c941dcfac6ae88.js.map
